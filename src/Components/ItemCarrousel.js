3abcba90dc97df36883e8e001837b1daa7df76d3
KennetQ2903 <kennetquiroz@gmail.com>
2023-05-04 18:37:46 -0600

    se agrego el catalogo con los items reales

--- src/Components/ItemCarrousel.js
+++ src/Components/ItemCarrousel.js
@@ -1,36 +1,17 @@
 import { ItemCard } from '@/Components/ItemCard'
+import { Catalogo } from '@/Config/Catalogo'
 import styles from '@/css/ItemCarrousel.module.css'
 export function ItemCarrousel () {
   return (
     <div className={styles.container}>
-      <ItemCard
-        title='Tensor de contorno de ojos'
-        price={20.34}
-      />
-      <ItemCard
-        title='Terapia frio/caliente'
-        price={20.34}
-      />
-      <ItemCard
-        title='Frecuencia galvánica'
-        price={20.34}
-      />
-      <ItemCard
-        title='Electroporador'
-        price={20.34}
-      />
-      <ItemCard
-        title='Gimnasia pasiva'
-        price={20.34}
-      />
-      <ItemCard
-        title='Cavitacion con radiofrecuencia'
-        price={20.34}
-      />
-      <ItemCard
-        title='Vaporizador de limpieza profunda'
-        price={20.34}
-      />
+      {Catalogo.map((item) => (
+        <ItemCard
+          key={item.title}
+          title={item.title}
+          price={20.34}
+          src={item.src}
+        />
+      ))}
     </div>
   )
 }
