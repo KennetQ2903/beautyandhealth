604de8e021ec25ed4a5840c85072b1cd9fb80775
KennetQ2903 <kennetquiroz@gmail.com>
2023-06-02 10:00:47 -0600

    add socials and fontawesome

---
+++ src/Config/RedesSociales.js
@@ -0,0 +1,5 @@
+export const redes = {
+  facebook: 'https://www.facebook.com/profile.php?id=100091566249448&mibextid=LQQJ4d',
+  instagram: 'https://instagram.com/beautyandhealth.__?igshid=MzRlODBiNWFlZA==',
+  whatsapp: 'https://api.whatsapp.com/send?phone=447710173736&text=Hola,%20estoy%20interesado%20en%20adquirir%20un%20producto.' // 'https://wa.me/447710173736?text=How%20are%20you%20?'
+}
